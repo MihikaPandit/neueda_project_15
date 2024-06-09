df48b819b96f10f6885399498288ace33d965623
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2024-06-09 23:24:24 +0530

    Add TransactionRecord class

--- src/main/java/com/example/library/model/TransactionRecord.java
+++ src/main/java/com/example/library/model/TransactionRecord.java
@@ -2,10 +2,17 @@ package com.example.library.model;
 
 public class TransactionRecord {
 
-    User user;
-    Entity entity;
+    private User user;
+    private Entity entity;
+    private String remark;
 
-    public TransactionRecord(User user, Entity entity) {
-        
+    public TransactionRecord(User user, Entity entity, String remark) {
+        this.user = user;
+        this.entity = entity;
+        this.remark = remark;
+    }
+
+    public String toString() {
+        return this.user + " transaction on \n" + this.entity + " \nRemark: " + this.remark;
     }
 }
