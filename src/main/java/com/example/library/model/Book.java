0370d9e492bd359cebe744d6f2f7eb6028417ff8
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2024-06-09 22:53:51 +0530

    Add book model

--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -1,5 +1,12 @@
 package com.example.library.model;
 
 public class Book extends Entity {
-    
+
+    public Book(String id, String name, boolean borrowable) {
+        super(id, name, borrowable);
+    }
+
+    public Book (String id, String name) {
+        super(id, name, true);
+    }
 }
