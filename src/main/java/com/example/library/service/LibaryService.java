b5ad61d96ef2a8028ccf16e2dac77cbe421c24d0
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2024-06-10 00:00:13 +0530

    Add method for borrow in library service

--- src/main/java/com/example/library/service/LibaryService.java
+++ src/main/java/com/example/library/service/LibaryService.java
@@ -3,12 +3,15 @@ package com.example.library.service;
 import com.example.library.model.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LibaryService{
 
     private List<Entity> items = new ArrayList<>();
     private List<User> users = new ArrayList<>();
+
+    private HashMap<String, TransactionRecord> borrowedList;
 
 
     public boolean addEntity(Entity entity) {
@@ -26,6 +29,17 @@ public class LibaryService{
         return null;
     }
 
+    public User findUserById(String id) {
+        for (User user : this.users) {
+            if (id.equals(user.getUserId())) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+
     public void viewInfo(String id) {
         Entity entity = findById(id);
         System.out.println(entity);
@@ -35,6 +49,40 @@ public class LibaryService{
     public void addUser(String userName, String userId){
        users.add(new User(userName, userId));
     }
+
+    public boolean borrow(String userId, String EntityId, String remark) {
+        Entity entity = this.findById(EntityId);
+
+        if (entity == null) {
+            System.out.println("Item not found");
+            return false;
+        }
+
+        if (!entity.isBorrowable()) {
+            System.out.println("Item Cannot be Borrowed");
+            return false;
+        }
+
+        if (entity.getIsBorrowed()) {
+            System.out.println("Not available in the library (already borrowed by someone)");
+            return false;
+        }
+
+        User user = findUserById(userId);
+
+        if (user == null) {
+            System.out.println("user not found");
+            return false;
+        }
+
+        TransactionRecord transactionRecord = new TransactionRecord(user, entity, remark);
+        entity.setIsBorrowed(true);
+        borrowedList.put(entity.getId(), transactionRecord);
+
+
+        return true;
+    }
+
 
     
 
