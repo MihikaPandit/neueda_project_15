2dc7e6f1ac17a45ea9da76b49d7c654ae3cb34d0
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2024-06-09 23:13:20 +0530

    Add transaction record

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,5 +1,6 @@
 package com.example.library;
 
+import com.example.library.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
+
+		Book book1 = new Book("8234234", "Blink - Malcolm Gladwell");
+		System.out.println(book1);
+
+		// SpringApplication.run(LibraryApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/example/library/model/TransactionRecord.java
@@ -0,0 +1,11 @@
+package com.example.library.model;
+
+public class TransactionRecord {
+
+    User user;
+    Entity entity;
+
+    public TransactionRecord(User user, Entity entity) {
+        
+    }
+}
