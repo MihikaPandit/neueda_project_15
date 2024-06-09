cfd6281cd73004e4a6628786077c198d3ba44000
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2024-06-10 00:28:51 +0530

    Add demonstration with all the transaction types

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,6 +1,8 @@
 package com.example.library;
 
 import com.example.library.model.Book;
+import com.example.library.model.User;
+import com.example.library.service.LibaryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +13,21 @@ public class LibraryApplication {
 
 		Book book1 = new Book("8234234", "Blink - Malcolm Gladwell");
 		System.out.println(book1);
+
+		User user1 = new User("Dhruv", "453fd");
+
+		LibaryService service = new LibaryService();
+
+		service.addEntity(book1);
+		service.addUser(user1);
+
+		service.borrow("453fd", "8234234", "Borrowed on 10/06/2024");
+
+		service.returnEntity("453fd", "8234234", "Book returned");
+
+		service.removeEntity("8234234");
+
+
 
 		// SpringApplication.run(LibraryApplication.class, args);
 
